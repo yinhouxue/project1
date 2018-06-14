86e6d26174778c5af1957df4dbf68d39f6142623
p552357111 <pankaiya552357111@qq.com>
2018-06-14 10:25:26 +0800

    第二次修改选项卡

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var oLi = document.getElementsByName("li");
 for(var i=0;i<oLi.length;i++){
     oLi[i].onclick=function(){
-        
+        for(var y=0;y<oLi.length;y++){
+        	oLi[y].className="";
+        }
+        oLi[this.index].className="red";
     }
 }
