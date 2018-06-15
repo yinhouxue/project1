8aea090cb85b1acf27910fd13cee083259f9a9ca
name <you@example.com>
2018-06-15 08:40:40 +0800

    在分支new2上修改了index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var oLi = document.getElementsByName("li");
+var oBox = document.getElementsByClassName("box");
 for(var i=0;i<oLi.length;i++){
     oLi[i].onclick=function(){
         for(var y=0;y<oLi.length;y++){
         	oLi[y].className="";
         }
         oLi[this.index].className="red";
+        for(var i=0;i<oBox.length;i++){
+            oBox[i].style.display = "none";
+        }
+        oBox[this.index].style.display = "block";
     }
 }
